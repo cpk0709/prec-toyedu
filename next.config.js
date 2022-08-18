0c5223eddea03b00b570f7786593bf478a8eb9fd
cpk0709 <cpk0709@gmail.com>
2022-08-18 12:05:07 +0900

    next.config.js .babelrc 설정

--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
+
+const withAntdLess = require("next-plugin-antd-less");
+
 const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
-}
+};
 
-module.exports = nextConfig
+module.exports = withAntdLess({
+  lessVarsFilePath: "./src/assets/variables.less",
+  ...nextConfig,
+});
