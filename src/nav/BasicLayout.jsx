84b3278ebcc1a31168ff075b9044beef874a6fca
cpk0709 <cpk0709@gmail.com>
2022-08-18 16:26:47 +0900

    nav 메뉴 설정

--- src/nav/BasicLayout.jsx
+++ src/nav/BasicLayout.jsx
@@ -1,6 +1,40 @@
 import React from "react";
 import Link from "next/link";
 
+const menu = [
+  {
+    key: "account",
+    title: "계정관리",
+    items: [
+      { item: "메뉴1", link: "/" },
+      { item: "메뉴2", link: "/" },
+      { item: "메뉴3", link: "/" },
+    ],
+  },
+  {
+    key: "bucket",
+    title: "버킷관리",
+    items: [
+      { item: "메뉴1", link: "/" },
+      { item: "메뉴2", link: "/" },
+      { item: "메뉴3", link: "/" },
+      { item: "메뉴4", link: "/" },
+    ],
+  },
+  { key: "sample1", title: "샘플메뉴1", link: "/" },
+  { key: "sample2", title: "샘플메뉴2", link: "/" },
+  {
+    key: "sample3",
+    title: "샘플메뉴3",
+    items: [
+      { item: "서브메뉴1", link: "/" },
+      { item: "서브메뉴2", link: "/" },
+      { item: "서브메뉴3", link: "/" },
+      { item: "서브메뉴4", link: "/" },
+    ],
+  },
+];
+
 const BasicLayout = ({ children }) => {
   return (
     <div>
