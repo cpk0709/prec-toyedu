93830c32a1cb7271510bb3d18c3e4fb26942d886
cpk0709 <cpk0709@gmail.com>
2022-08-18 11:43:33 +0900

    useAxios 코드 추가

--- src/hooks/useAxios.js
+++ src/hooks/useAxios.js
@@ -1,1 +1,31 @@
 import React from "react";
+
+const useAxios = () => {
+  const [state, setState] = React.useState({
+    data: null,
+    error: null,
+    loading: false,
+  });
+
+  const loadData = async (axios) => {
+    setState({ data: null, error: null, loading: true });
+    try {
+      const responseData = await axios;
+      if (responseData) {
+        setState({ data: responseData.data, error: null, loading: false });
+        return responseData;
+      }
+    } catch (error) {
+      setState((prevState) => ({ ...prevState, error, loading: false }));
+      throw error;
+    }
+  };
+
+  const clear = () => {
+    setState({ data: null, error: null, loading: false });
+  };
+
+  return { ...state, loadData, clear };
+};
+
+export default useAxios;
