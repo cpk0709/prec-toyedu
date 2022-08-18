1e664e2a2e0c0e5be8a24ba301ae81d0ec784bf7
cpk0709 <cpk0709@gmail.com>
2022-08-18 16:49:47 +0900

    나중에 로그인 여부에 따라 다르게 보이기 위한 초석

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -1,7 +1,28 @@
-import '../styles/globals.css'
+import "../styles/globals.css";
+import BasicLayout from "../nav/BasicLayout";
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
-}
+//로그인 했을 때와 안 했을 때 다른 화면이 보이는 것을 가정
+const MainContainer = (props) => {
+  const { Component, router, pageProps } = props;
+  // 나중에 로그인 값 어떻게 불러올지 구상해서 작성 예정
+  // const isLogin = useSelector();
 
-export default MyApp
+  // if(!isLogin){
+  //   return(
+  //     <AnotherLayout>
+  //       <Login {...PageProps}/>
+  //     </AnotherLayout>
+  //   )
+  // }
+  return (
+    <BasicLayout>
+      <Component {...pageProps} />
+    </BasicLayout>
+  );
+};
+
+const MyApp = (props) => {
+  return <MainContainer {...props} />;
+};
+
+export default MyApp;
