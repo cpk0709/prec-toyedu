9e4d073a9fd644e9c1b3501930a8d45ed241ea63
cpk0709 <cpk0709@gmail.com>
2022-08-18 14:12:46 +0900

    메인페이지 헤더 틀 작성

--- src/pages/account/index.jsx
+++ src/pages/account/index.jsx
@@ -1,9 +1,34 @@
+import React from "react";
+import { Table, Button, Typography } from "antd";
+import styled from "styled-components";
+
 const MainPageIndex = () => {
   return (
-    <div>
+    <MainWrapper>
+      <MainHeader>
+        <MainTitle>
+          <Typography.Title level={2}>버킷 리스트</Typography.Title>
+        </MainTitle>
+      </MainHeader>
       <p>Main Page</p>
-    </div>
+    </MainWrapper>
   );
 };
 
+const MainWrapper = styled.div`
+  flex: 1;
+  margin-left: 36px;
+  margin-right: 120px;
+`;
+
+const MainHeader = styled.div`
+  display: flex;
+  flex-direction: column;
+  margin-top: 40px;
+`;
+
+const MainTitle = styled.div`
+  display: flex;
+  flex-direction: row;
+`;
 export default MainPageIndex;
