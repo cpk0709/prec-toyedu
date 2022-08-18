54d0813bfd1766464e20d0a3a2697f487d3c2d10
cpk0709 <cpk0709@gmail.com>
2022-08-18 16:18:53 +0900

    BasicLayout 기본 틀 작성

---
+++ src/nav/BasicLayout.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Link from "next/link";
+
+const BasicLayout = ({ children }) => {
+  return (
+    <div>
+      <p>Basic Layout</p>
+    </div>
+  );
+};
+
+export default BasicLayout;
