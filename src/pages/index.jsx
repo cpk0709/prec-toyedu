8c2a4198e33d99c9b2b40363e11adb8dee470d99
cpk0709 <cpk0709@gmail.com>
2022-08-18 22:32:29 +0900

    시작 페이지 경로 수정

--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -5,7 +5,8 @@ const Index = () => {
   const router = useRouter();
 
   useEffect(() => {
-    router.replace("/account");
+    // router.replace("/account");
+    router.replace("/bucket/show");
   }, []);
 };
 export default Index;
